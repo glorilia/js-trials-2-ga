bb258aceb081fd6a5853973fb7909029d1289090
Glorili Alejandro <gloriliale@gmail.com>
2020-07-28 23:58:02 +0000

    Completed getFullName function.

--- functions.js
+++ functions.js
@@ -4,13 +4,15 @@
 // 1. isHometown
 
 function isHometown(town) {
-    return town === 'San Francisco'
+    return town === 'San Francisco';
 }
 
 
 // 2. getFullName
 
-// Define your function here
+function getFullName(firstName, lastName) {
+    return `${firstName} ${lastName}`;
+}
 
 
 // 3. calculateTotal
