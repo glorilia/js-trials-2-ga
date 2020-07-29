0c84f88d93c0aa1438b068ea631d9dd156d692ef
Glorili Alejandro <gloriliale@gmail.com>
2020-07-29 00:09:22 +0000

    Completed the calculateTotal function.

--- functions.js
+++ functions.js
@@ -17,4 +17,19 @@ function getFullName(firstName, lastName) {
 
 // 3. calculateTotal
 
-// Define your function here
+function calculateTotal(basePrice, state, tax=0.05) {
+    const subtotal = basePrice * (1 + tax);
+    let fee = 0;
+    if (state === 'CA') {
+        fee = 0.03 * subtotal;
+    } else if (state === 'PA') {
+        fee = 2;
+    } else if (state === 'MA') {
+        if (basePrice <= 100) {
+            fee = 1;
+        } else {
+            fee = 3;
+        }
+    }
+    return subtotal + fee
+}
