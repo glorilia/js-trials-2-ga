1e954a26d866c7a2bd1dc64da224ebafa5e5c247
Glorili Alejandro <gloriliale@gmail.com>
2020-07-29 00:31:32 +0000

    Completed the getMelonsAtPrice function.

--- objects.js
+++ objects.js
@@ -18,5 +18,14 @@ function countWords(phrase) {
 
 // 2. getMelonsAtPrice
 function getMelonsAtPrice(price) {
-  // Replace this with your code
+    const melonPrices = {
+        2.50: ['Cantaloupe', 'Honeydew'],
+        2.95: ['Watermelon'],
+        3.25: ['Musk', 'Crenshaw'],
+        14.25: ['Christmas']
+    };
+    if (!melonPrices.hasOwnProperty(price)) {
+        return
+    }
+    return melonPrices[price].sort()
 }
